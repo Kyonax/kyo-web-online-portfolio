1d40f5ec424ecae8977b6aeb8396dd541e2149a0
Kyonax <kyonax.corp@gmail.com>
2023-10-25 23:34:24 -0500

    Basics

--- src/body/nav/NavBar.tsx
+++ src/body/nav/NavBar.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom'
+import { motion, useAnimation } from 'framer-motion';
 
 import HackerEffect from '../../misc/functions/HackerEffect';
+import SlideAnimation from '../../misc/functions/SlideAnimation';
 
 import './NavBar.css'
 
@@ -11,29 +13,33 @@ interface NavBarProps {
 }
 
 const NavBar: React.FC<NavBarProps> = ({ colorPalette, setIndexPalette }) => {
-    useEffect(() => { const hackerEffect = HackerEffect('hacker', 7) }, [])
+    const { animationControls } = SlideAnimation(useAnimation);
     
+    useEffect(() => { const hackerEffect = HackerEffect('hacker', 7) }, [])    
     return (
         <div style={{ backgroundColor: colorPalette.background, color: colorPalette.text,
                       borderColor: colorPalette.text }} className="main-container">
 
             <div style={{ borderColor: colorPalette.text }} className="container">
                 <div className="link-container">
-                    <Link className="link group" to="/" >
-                        <div className="bg-reveal"/>
-                        <p className="link-ghost" id="hacker" data-value="ABOUT">ABOUT</p> 
+
+                    <Link className="link" to="/" >
+                        <motion.div
+                            animate={ animationControls }
+                            className="bg-reveal"/>
                         <p id="hacker" data-value="ABOUT">ABOUT</p> 
                     </Link>
-                    <Link className="link group" to="/" >
-                        <div className="bg-reveal"/>
-                        <p className="link-ghost" id="hacker" data-value="WORK">WORK</p> 
+
+                    <Link className="link" to="/" >
+                        <motion.div className="bg-reveal"/>
                         <p id="hacker" data-value="WORK">WORK</p> 
                     </Link>
-                    <Link className="link group" to="/" >
-                        <div className="bg-reveal"/>
-                        <p className="link-ghost" id="hacker" data-value="CONTACT">CONTACT</p> 
+
+                    <Link className="link" to="/" >
+                        <motion.div className="bg-reveal"/>
                         <p id="hacker" data-value="CONTACT">CONTACT</p> 
                     </Link>
+
                 </div>
                 <div className="w-2/3"></div>
             </div>
