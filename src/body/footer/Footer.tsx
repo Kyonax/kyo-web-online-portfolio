0653b5d2f8a31ef5707b5b8a3b170068797f530b
Kyonax <kyonax.corp@gmail.com>
2024-02-25 18:04:41 -0500

    New Version of Development

--- src/body/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-interface FooterProps {}
-
-const Footer: React.FC<FooterProps> = ({}) => {
-    return (
-        <div><h1>This is the Footer Element</h1></div>
-    )
-}
-
-export default Footer
--- src/body/home/Home.tsx
+++
@@ -1,9 +0,0 @@
-interface HomeProps {}
-
-const Home: React.FC<HomeProps> = ({}) => {
-    return (
-        <div><h1>This is the Home Element</h1></div>
-    )
-}
-
-export default Home
